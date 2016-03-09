de0d1ffd6b517e49ad579f49abc107d94211fe78
TeddyZhang <993014120@qq.com>
2016-03-10 00:14:45 +0800

    define UI-class GameView

--- src/main/java/com/example/myapplication/GameView.java
+++ src/main/java/com/example/myapplication/GameView.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridLayout;
 
 /**
@@ -27,6 +29,36 @@ public class GameView extends GridLayout {
 //        super(context, attrs, defStyleAttr, defStyleRes);
 //    }
     private void initGameView( ) {
+        setOnTouchListener(new OnTouchListener() {
+            private float startX,startY,offsetX,offsetY;
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startX = motionEvent.getX();
+                        startY = motionEvent.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        offsetX=motionEvent.getX()-startY;
+                        offsetY=motionEvent.getY()-startY;
 
+                        if(Math.abs(offsetX)>Math.abs(offsetY)) {
+                            if(offsetX<-5) {
+                                System.out.println("left");
+                            }
+                            else if (offsetX>-5)
+                                System.out.println("right");
+                        }else {
+                            if (offsetY<-5) {
+                                System.out.println("up");
+                            }
+                            else if (offsetY>-5)
+                                System.out.println("down");
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
